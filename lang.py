8fe3e57b4858c8c10fad45e1e8038a04e741b486
Rakshith <Rakshith@x-10-104-176-21.uofm-secure.wireless.umn.edu>
2017-01-29 07:21:14 -0600

    Modified to use with python2

--- lang.py
+++ lang.py
@@ -1,9 +1,12 @@
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-
+senten[]
 sentence="This is where the message goes"
 stop_word=set(stopwords.words("english"))
 word=word_tokenize(sentence)
+for w in range:
+        if (w != stop_word)
+                senten=w
 senten=[w for w in range if not in stop_word]
 
 sym= ["cough", "difficulty breathing", "nasal congestion", "muscle ache", "fatigue", "cold", "fever", "spasms", "body pain", "nausea", "abdominal pain", "diarrhea", "seizures","inflamed eyes", "red skin rash", "sore throat", "weight loss", "night sweat"]
