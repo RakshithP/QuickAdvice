3d2bbcc2eb9eda596ca03d54da8c1bbc4f3d4fae
Rakshith <Rakshith@x-10-104-176-21.uofm-secure.wireless.umn.edu>
2017-01-29 04:12:22 -0600

    added function to extract disease

--- routes/disease/disease.controller.js
+++ routes/disease/disease.controller.js
@@ -24,7 +24,37 @@
             res.json(dataResult);
         });
     };
+    
+    module.exports.getBySymptom = function(req, res) { 
 
+        console.log("DATA SENT: " + req.params.symptom);
+        //before I look in the database
+
+
+        Disease.find(
+            {
+                "Symptoms" : 
+                {
+                    $in : [req.params.symptom]
+                } 
+            }
+            , function (err, dataResult) {
+
+            if (err) {
+                console.error(err);
+                return res.status(500).send(err);
+            }
+
+            //after I looked in the database
+
+            if(dataResult.length < 1) {
+                return res.send("No disease found");
+            } 
+            //console.dir(dataResult);
+            //res.send(dataResult[0]._doc.Treatment);
+            return res.send(dataResult);
+        });
+    };
         
         
     
--- routes/disease/index.js
+++ routes/disease/index.js
@@ -6,9 +6,9 @@
     
   var router = express.Router();
 
-  router.get('/getAll', controller.getAll);  
-  router.get('/name/:disease', controller.getByName);  
-  //router.get('/symptom/:symptom', controller.getBySymptom);  
+  //router.get('/getAll', controller.getAll);  
+  //router.get('/name/:disease', controller.getByName);  
+  router.get('/symptom/:symptom', controller.getBySymptom);  
   //router.post('/new', controller.newDisease);  
 
   module.exports = router;
