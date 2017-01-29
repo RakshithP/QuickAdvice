8d446bcc3f561c11048d84fd04943b0c2a5f4d4e
Tushar <tushar.n22@gmail.com>
2017-01-29 12:38:08 -0600

    version 1.0

--- lang.py
+++ lang.py
@@ -1,18 +1,20 @@
+import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-senten[]
-sentence="This is where the message goes"
+import sys
+a= []
+senten = sys.argv[1].lower()
+fin_sentence = []
 stop_word=set(stopwords.words("english"))
-word=word_tokenize(sentence)
-for w in range:
-        if (w != stop_word)
-                senten=w
-senten=[w for w in range if not in stop_word]
+word=word_tokenize(senten)
+
+for w in word:
+        if (w not in stop_word):
+                a.append(w)
+print(a)        
+
 
 sym= ["cough", "difficulty breathing", "nasal congestion", "muscle ache", "fatigue", "cold", "fever", "spasms", "body pain", "nausea", "abdominal pain", "diarrhea", "seizures","inflamed eyes", "red skin rash", "sore throat", "weight loss", "night sweat"]
-for i in senten:
-	for j in sym:
-		if(senten[i]==sym[j])
-			fin_sentence = senten[i]
 
 
+sys.stdout.flush()
--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var twilio = require('twilio')("AC87b33b99abdbf60842b57ebcac3b0a73", "9a106c6c08
 var bodyParser = require("body-parser");
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.json());
 
 //var api = require('./routes/api.js'); //gets api logic from path
 
@@ -22,50 +23,65 @@ MongoDB.once('open', function() {
 //use ngrok to generate link to be placed in twilio's app dashboard, or host on public server 
 //post request to send sms to twilio owned number 
 app.post("/message", function (req, res) {
-  var i;
+  console.log("check");
   var twilio = require('twilio');
+  var twiml = new twilio.TwimlResponse();
   var Disease = require('./routes/disease/disease.model.js');
+  var messageBody; 
   
   //before I look in the database
-  console.log(req.body.Body + "\n" + req.body.From + "\n");
-  Disease.find(
-	{
-		"Symptoms" : 
-			{
-                $in : [req.params.symptom]
-            } 
-    },function (err, dataResult) {
+  console.log(req.body.Body + "\n" + req.body.From + "\n"); 
+  console.dir(req.body);
+  var python = require('child_process').spawn('python3', ["./lang.py", req.body.Body] );  // second argument is array of parameters, e.g.:
+  var output = [];
+  python.stdout.on('data', function(data){output.concat(data)});
+  python.on('close', function(code){ 
+  if (code !== 0) {  
+    return res.send(500, code); 
+  }
+     });
+   // } else { res.send(500, 'No file found') }
+	Disease.find(
+		{
+			"Symptoms" : 
+				{
+					$in : output
+				} 
+		},function (err, dataResult) {
+            //after I looked in the database
+			
+			//on error
 			if (err) {
                 console.error(err);
                 return res.status(500).send(err);
             }
 
-            //after I looked in the database
-
+			// check if empty query result
             if(dataResult.length < 1) {
-                return res.send("No disease found");
-            } 
-            //console.dir(dataResult);
-            //res.send(dataResult[0]._doc.Treatment);
-            return res.send(dataResult);
-        });
-    };
-	for(i=0; i< dataResult.length; i++){
-			console.log(dataResult[i].Disease);
-	}
-   });
-    
-  var twiml = new twilio.TwimlResponse();
-  twiml.message(function(){
-		this.body('Here is your query');
-	 });
-  res.writeHead(200, {'Content-Type': 'text/xml'});
-  res.end(twiml.toString()); 
+				console.log("NO DATA FOUND");
+				messageBody = "No diseases found"; 
+            } else{
+				console.log(dataResult);
+				for(var i=0; i< dataResult.length; i++){
+					console.log(dataResult[i].Disease);
+					messageBody = dataResult[i].Disease + " ";
+				}
+			}
+			
+			console.log("warning " + dataResult[0]); 
+			twiml.message(function(){
+				console.log(messageBody);
+				this.body(messageBody);
+				console.log("after body");
+			});
+			console.log("sending res");
+			res.writeHead(200, {'Content-Type': 'text/xml'});
+			res.end(twiml.toString());  
+		});
 });
 
 app.get("/", function(req,res){
 	var Disease = require('./routes/disease/disease.model.js');
-	var i;
 	console.log("Test Run");
 	Disease.find({}, function (err, dataResult) {
 		if (err) {
@@ -73,11 +89,12 @@ app.get("/", function(req,res){
 			return res.status(500).send(err);
 		}
 		console.log(dataResult);
-		for(i=0; i< dataResult.length; i++){
+		for(var i=0; i< dataResult.length; i++){
 			console.log(dataResult[i].Disease);
-	    }
-		return res.json(dataResult);
+	}
+	return res.json(dataResult);
 	});
+	
 });
  
 http.createServer(app).listen(8080, function () {
